8ae954ea9b3204f9175ef0d3cbc048ef7b3fc5f9
MattJarman <mattjarman22@outlook.com>
2022-06-21 22:24:26 +0100

    Add game queries for single game

--- src/lib/contentful/game.ts
+++ src/lib/contentful/game.ts
@@ -1,6 +1,7 @@
 import { ContentfulGraphQLResponse, query } from './query'
 
 export type Game = {
+  slug: string
   title: string
   rating: number
   completedAt: string
@@ -11,46 +12,83 @@ export type Game = {
     title: string
     url: string
   }
-  playtime: number
+  playtime?: number
   tags: string[]
   notes: {
     json: unknown
   }
-  completionStats: string
-  difficulty: string
+  completionStats?: string
+  difficulty?: string
 }
 
 type GameGraphQLResponse = ContentfulGraphQLResponse<'gameCollection', Game>
 
+const GAME_GRAPHQL_FIELDS = `
+slug
+sys {
+  id
+}
+title
+rating
+completedAt
+image {
+  title
+  url
+}
+playtime
+tags 
+notes {
+  json
+}
+completionStats
+difficulty`
+
 const extractGameEntries = (entries: GameGraphQLResponse) =>
   entries?.data?.gameCollection?.items || []
+
+const extractGame = (entries: GameGraphQLResponse) =>
+  entries?.data?.gameCollection?.items?.[0]
 
 export const getAllGames = async (): Promise<Game[]> => {
   const entries = await query<GameGraphQLResponse>(
     `query {
         gameCollection(order: completedAt_DESC) {
           items {
-            sys {
-              id
-            }
-            title
-            rating
-            completedAt
-            image {
-              title
-              url
-            }
-            playtime
-            tags 
-            notes {
-              json
-            }
-            completionStats
-            difficulty
+            ${GAME_GRAPHQL_FIELDS}
           }
         }
       }`
   )
 
   return extractGameEntries(entries)
+}
+
+export const getAllGamesWithSlug = async (): Promise<Game[]> => {
+  const entries = await query<GameGraphQLResponse>(
+    `query {
+        gameCollection(where: { slug_exists: true }, order: completedAt_DESC) {
+          items {
+            ${GAME_GRAPHQL_FIELDS}
+          }
+        }
+      }`
+  )
+
+  return extractGameEntries(entries)
+}
+
+export const getGameBySlug = async (
+  slug: string
+): Promise<Game | undefined> => {
+  const entries = await query<GameGraphQLResponse>(
+    `query {
+        gameCollection(where: { slug: "${slug}" }, limit: 1) {
+          items {
+            ${GAME_GRAPHQL_FIELDS}
+          }
+        }
+      }`
+  )
+
+  return extractGame(entries)
 }
--- test/unit/lib/contentful/game.spec.ts
+++ test/unit/lib/contentful/game.spec.ts
@@ -1,12 +1,16 @@
-import { getAllGames } from 'src/lib/contentful/game'
-import { query } from 'src/lib/contentful/query'
+import {
+  getAllGames,
+  getAllGamesWithSlug,
+  getGameBySlug,
+  query
+} from 'src/lib/contentful'
 
 jest.mock('src/lib/contentful/query')
 
 const mockedQuery = query as jest.MockedFunction<typeof query>
 
 describe('Test getAllGames', () => {
-  it('returns all games if all keys are defined in response', async () => {
+  it('returns all games if all keys are defined in the response', async () => {
     const expected = [{ id: '29428' }]
     const entries = {
       data: {
@@ -29,5 +33,51 @@ describe('Test getAllGames', () => {
     const actual = await getAllGames()
 
     expect(actual).toEqual([])
+  })
+})
+
+describe('Test getAllGamesWithSlug', () => {
+  it('returns all games if all keys are defined in the response', async () => {
+    const expected = [{ id: '29428' }]
+    const entries = {
+      data: {
+        gameCollection: {
+          items: expected
+        }
+      }
+    }
+
+    mockedQuery.mockResolvedValueOnce(entries)
+
+    const actual = await getAllGamesWithSlug()
+
+    expect(actual).toEqual(expected)
+  })
+})
+
+describe('Test getGameBySlug', () => {
+  it('returns game if all keys are defined in the response', async () => {
+    const expected = { id: '29428' }
+    const entries = {
+      data: {
+        gameCollection: {
+          items: [expected]
+        }
+      }
+    }
+
+    mockedQuery.mockResolvedValueOnce(entries)
+
+    const actual = await getGameBySlug('slug')
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('returns undefined if one of the keys in the response was not defined', async () => {
+    mockedQuery.mockResolvedValueOnce({})
+
+    const actual = await getGameBySlug('slug')
+
+    expect(actual).toBeUndefined()
   })
 })
