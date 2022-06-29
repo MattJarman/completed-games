9a702ae11cb2dd4017258191a71f80d2ee098eaf
MattJarman <mattjarman22@outlook.com>
2022-06-29 17:53:30 +0100

    Add home page test

---
+++ test/unit/pages/index.spec.tsx
@@ -0,0 +1,50 @@
+import { render } from '@testing-library/react'
+import GameCard from 'src/components/atoms/game'
+import { Game, getAllGames } from 'src/lib/contentful'
+import HomePage, { getStaticProps } from 'src/pages/index'
+
+jest.mock('src/lib/contentful')
+jest.mock('src/components/atoms/game')
+
+const mockGetAllGames = getAllGames as jest.MockedFunction<typeof getAllGames>
+const mockGameCard = GameCard as jest.MockedFunction<typeof GameCard>
+
+const game = {
+  sys: {
+    id: '123'
+  },
+  slug: 'resident-evil-4',
+  title: 'Resident Evil 4',
+  image: {
+    url: '/resident-evil-4-img'
+  },
+  rating: 90
+}
+
+describe('HomePage', () => {
+  describe('HomePage rendering', () => {
+    it('renders the home page correctly', () => {
+      render(<HomePage allGames={[game] as unknown as Game[]} />)
+
+      expect(mockGameCard).toHaveBeenCalledWith(
+        {
+          id: game.sys.id,
+          img: game.image.url,
+          rating: game.rating,
+          slug: game.slug,
+          title: game.title
+        },
+        {}
+      )
+    })
+  })
+
+  describe('getStaticProps', () => {
+    it('returns allGames as props', async () => {
+      mockGetAllGames.mockResolvedValueOnce([game] as unknown as Game[])
+      const result = await getStaticProps({})
+      console.log(result)
+      expect(result).toEqual({ props: { allGames: [game] } })
+    })
+  })
+})
