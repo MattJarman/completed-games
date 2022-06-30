8052f8d8972a51105b5f4c0e9b1510c0463cabfb
MattJarman <mattjarman22@outlook.com>
2022-06-30 19:14:33 +0100

    Fix missing hex value

--- src/lib/utils/getRatingColour.ts
+++ src/lib/utils/getRatingColour.ts
@@ -1,7 +1,7 @@
 export const getRatingColour = (rating: number) => {
   if (rating >= 95) return '#0EA5E9'
   if (rating > 80 && rating < 95) return '#22C55E'
-  if (rating > 75 && rating <= 80) return '86EFAC'
+  if (rating > 75 && rating <= 80) return '#86EFAC'
   if (rating > 60 && rating <= 75) return '#FACC15'
   if (rating > 50 && rating <= 60) return '#EAB308'
 
