fd1e45dc86993627b533c8bf14dbd935d0f3260a
MattJarman <mattjarman22@outlook.com>
2022-07-26 21:31:05 +0100

    Add tests

--- test/unit/pages/index.spec.tsx
+++ test/unit/pages/index.spec.tsx
@@ -1,13 +1,17 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { debounce } from 'lodash'
 import GameCard from 'src/components/atoms/gameCard'
 import { Game, getAllGames } from 'src/lib/contentful'
 import HomePage, { getStaticProps } from 'src/pages/index'
 
 jest.mock('src/lib/contentful')
 jest.mock('src/components/atoms/gameCard')
+jest.mock('lodash')
 
 const mockGetAllGames = getAllGames as jest.MockedFunction<typeof getAllGames>
 const mockGameCard = GameCard as jest.MockedFunction<typeof GameCard>
+const mockDebounce = debounce as jest.MockedFunction<typeof debounce>
 
 const game = {
   sys: {
@@ -21,7 +25,25 @@ const game = {
   rating: 90
 }
 
+const game2 = {
+  sys: {
+    id: '456'
+  },
+  slug: 'risk-of-rain-2',
+  title: 'Risk Of Rain 2',
+  image: {
+    url: '/risk-of-rain-2-img'
+  },
+  rating: 95
+}
+
 describe('HomePage', () => {
+  beforeAll(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    mockDebounce.mockImplementation((fn) => fn)
+  })
+
   describe('HomePage rendering', () => {
     it('renders the home page correctly', () => {
       render(<HomePage allGames={[game] as unknown as Game[]} />)
@@ -36,6 +58,19 @@ describe('HomePage', () => {
         },
         {}
       )
+    })
+
+    it('renders the home page correctly', async () => {
+      const user = userEvent.setup()
+      const testId = 'game-card'
+      mockGameCard.mockReturnValue(<div data-testid="game-card"></div>)
+      render(<HomePage allGames={[game, game2] as unknown as Game[]} />)
+
+      expect(screen.getAllByTestId(testId).length).toEqual(2)
+
+      await user.type(screen.getByPlaceholderText('Search'), 'Res')
+
+      expect(screen.getAllByTestId(testId).length).toEqual(1)
     })
   })
 
