bfd9cbe06c887078a0abefaaa17f77c466f2eda6
MattJarman <mattjarman22@outlook.com>
2022-12-17 15:31:17 +0000

    Change Layout typing

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -1,7 +1,6 @@
-import type { NextPage } from 'next'
-import type { PropsWithChildren } from 'react'
+import type { FC, PropsWithChildren } from 'react'
 
-const Layout: NextPage<PropsWithChildren> = ({ children }) => (
+const Layout: FC<PropsWithChildren> = ({ children }) => (
   <div className="flex flex-col min-h-screen transition-colors duration-300 bg-gray-900">
     <main className="container flex-grow px-4 mx-auto mb-auto lg:px-32">
       {children}
